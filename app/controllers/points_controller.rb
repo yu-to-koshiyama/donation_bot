2796ed9527b0171adf8e7547ddcac7e52bc6b05a
Yuto Koshiyama <yu_tok54@icloud.com>
2020-08-18 11:37:25 +0900

    set-Basic-auth

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,4 +1,6 @@
 class PointsController < ApplicationController
+  before_action :basic_auth
+
   def index
     @points = Point.all
     point_array
@@ -34,7 +36,11 @@ class PointsController < ApplicationController
       points << point_hash
     end
   end
-
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['AIAU_BASIC_AUTH_USER'] && password == ENV['AIAU_BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
+    end
+  end
 
 
 
